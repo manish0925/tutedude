bfe4ea4a6641324f5d30210c08a83040cef2e909
manish0925 <156315401+manish0925@users.noreply.github.com>
2025-10-22 05:43:21 +0530

    Add files via upload

---
+++ assignment1/task1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+sum=num1+num2
+substraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("Addition:",sum)
+print("Substraction:",substraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
